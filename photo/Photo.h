a1a753ff0a1d6045e8fb4facead98e9a5729ba4f
Piotr Zolnierek <pz@anixe.pl>
2012-02-19 20:47:34 +0100

    Add missing file (objc 1.0)

--- photo/Photo.h
+++ photo/Photo.h
@@ -1,11 +1,13 @@
-#import <Foundation/NSObject.h>
+#import <Cocoa/Cocoa.h>
 
 @interface Photo : NSObject {
-  NSString* caption;
-  NSString* photographer;  
+    NSString* caption;
+    NSString* photographer;
 }
+- (NSString*) caption;
+- (NSString*) photographer;
 
-@property (retain) NSString* caption;
-@property (retain) NSString* photographer;
+- (void) setCaption: (NSString*)input;
+- (void) setPhotographer: (NSString*)input;
 
 @end
